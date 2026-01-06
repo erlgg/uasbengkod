0b64093a6bdc27316e71797cfcfa74419ff877aa
erlgg <erlanggahernawan@gmail.com>
2026-01-07 06:30:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def user_input_features():
     dependents = st.sidebar.selectbox('Punya Tanggungan (Dependents)?', ['Yes', 'No'])
     contract = st.sidebar.selectbox('Kontrak', ['Month-to-month', 'One year', 'Two year'])
     paperless = st.sidebar.selectbox('Tagihan Paperless?', ['Yes', 'No'])
-    
+tenure, monthly_usd, total_usd, monthly_idr, total_idr = user_input_features()    
     data = {
         'tenure': tenure,
         'MonthlyCharges': monthly_charges,
@@ -30,7 +30,7 @@ def user_input_features():
     }
     features = pd.DataFrame(data, index=[0])
     return features
-tenure, monthly_usd, total_usd, monthly_idr, total_idr = user_input_features()
+
 input_df = user_input_features()
 st.subheader('Data Pelanggan')
 st.write(input_df)
@@ -62,4 +62,5 @@ if st.button('Prediksi Sekarang'):
     else:
         st.success(f" PREDIKSI: AMAN (Setia)")
         st.write("Pelanggan terlihat puas dengan layanan.")
+
 
